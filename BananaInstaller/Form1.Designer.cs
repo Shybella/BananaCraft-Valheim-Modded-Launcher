f4f6e8f0f018bee424e886425a3d641abaa06baf
Nicholas Tow <nxtr1114@gmail.com>
2021-03-31 13:34:21 -0500

    Add project files.

---
+++ BananaInstaller/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace BananaInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.uninstallToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.supportToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.launchArgumentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 103);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.MaxLength = 3276700;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(442, 219);
+            this.textBox1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uninstallToolStripMenuItem1,
+            this.supportToolStripMenuItem1,
+            this.launchArgumentsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(630, 33);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // uninstallToolStripMenuItem1
+            // 
+            this.uninstallToolStripMenuItem1.Name = "uninstallToolStripMenuItem1";
+            this.uninstallToolStripMenuItem1.Size = new System.Drawing.Size(95, 29);
+            this.uninstallToolStripMenuItem1.Text = "Uninstall";
+            this.uninstallToolStripMenuItem1.Click += new System.EventHandler(this.uninstallToolStripMenuItem1_Click);
+            // 
+            // supportToolStripMenuItem1
+            // 
+            this.supportToolStripMenuItem1.Name = "supportToolStripMenuItem1";
+            this.supportToolStripMenuItem1.Size = new System.Drawing.Size(93, 29);
+            this.supportToolStripMenuItem1.Text = "Support";
+            this.supportToolStripMenuItem1.Click += new System.EventHandler(this.supportToolStripMenuItem1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BananaInstaller.Properties.Resources.viking_banana;
+            this.pictureBox1.Location = new System.Drawing.Point(460, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 243);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 282);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(442, 42);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Status: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 349);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(442, 26);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Server Auto-Connect:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(303, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Ex - 127.0.0.1:2456";
+            // 
+            // launchArgumentsToolStripMenuItem
+            // 
+            this.launchArgumentsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.launchArgumentsToolStripMenuItem.Name = "launchArgumentsToolStripMenuItem";
+            this.launchArgumentsToolStripMenuItem.Size = new System.Drawing.Size(176, 29);
+            this.launchArgumentsToolStripMenuItem.Text = "Launch Arguments";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 31);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 395);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bananacraft Valheim Mod Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem uninstallToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem supportToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem launchArgumentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+    }
+}
+
---
+++ BananaInstaller/Form1.cs
@@ -0,0 +1,317 @@
+﻿using Ionic.Zip;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BananaInstaller
+{
+    public partial class Form1 : Form
+    {
+        string url = "http://play.bananaservers.com/Mods.zip";
+        private static System.Threading.Timer _timer;
+        public delegate void InvokeDelegate();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _timer = new System.Threading.Timer(Callback, null, 2500, Timeout.Infinite);
+            textBox1.AppendText(" -- Bananacraft Valheim Mod Installer --" + Environment.NewLine);
+            button1.Enabled = false;
+            button1.Text = "LOADING";
+            label1.Text = "Status: Idle";
+            textBox2.Text = Properties.Settings.Default.Server;
+            toolStripTextBox1.Text = Properties.Settings.Default.Arguments;
+            update_mods();
+        }
+        private void not_installed()
+        {
+            _timer = new System.Threading.Timer(Callback, null, 2500, Timeout.Infinite);
+        }
+
+        private async Task update_mods()
+        {
+            if (!File.Exists(Properties.Settings.Default.Path + @"\\Mods.zip"))
+            {
+                textBox1.AppendText("-> Mods update pending" + Environment.NewLine);
+                Properties.Settings.Default.update = true;
+                Properties.Settings.Default.Save();
+                return;
+            }
+            var request = HttpWebRequest.CreateHttp(url);
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1";
+            request.Method = "HEAD";
+            using (var response = await request.GetResponseAsync())
+            {
+                label1.Text = "Status: Checking for mod updates..";
+                var length_web = response.ContentLength;
+                var length_system = new System.IO.FileInfo(Properties.Settings.Default.Path + @"\\Mods.zip").Length;
+                
+                if (length_system == length_web)
+                {
+                    textBox1.AppendText("-> Running latest mods" + Environment.NewLine);
+                    Properties.Settings.Default.update = false;
+                    Properties.Settings.Default.Save();
+                }
+                else
+                {
+                    textBox1.AppendText("-> Mods update pending" + Environment.NewLine);
+                    Properties.Settings.Default.update = true;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void StartGame()
+        {
+            var p = new Process();
+            p.StartInfo.FileName = Properties.Settings.Default.Path + "\\valheim.exe";
+            p.StartInfo.Arguments = "+connect " + textBox2.Text + " " + toolStripTextBox1.Text;
+            p.StartInfo.RedirectStandardOutput = false;
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+
+        }
+        private void CheckProcess(bool kill)
+        {
+            if (Process.GetProcessesByName("valheim").Length > 0)
+            {
+                var procList = Process.GetProcesses().Where(process => process.ProcessName.Contains("valheim"));
+                foreach (var process in procList)
+                {
+                    label1.Text = "Status: Checking for process..";
+                    string valheimpath = Path.GetDirectoryName(process.MainModule.FileName);
+                    Properties.Settings.Default.Path = valheimpath;
+                    Properties.Settings.Default.Save();
+                    label1.Text = "Killing Process";
+                    if(kill == true)
+                    {
+
+                    }
+                    process.Kill();
+                }
+            }
+        }
+               
+        private void Callback(Object state)
+        {
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            textBox1.BeginInvoke(new InvokeDelegate(InvokeMethod));
+            _timer.Change(Math.Max(0, 3000 - watch.ElapsedMilliseconds), Timeout.Infinite);
+        }
+
+        public void InvokeMethod()
+        {
+            if(Properties.Settings.Default.Path == "")
+            {
+                CheckProcess(true);
+                textBox1.AppendText("-> Valheim not found! Please launch valheim!" + Environment.NewLine);
+                label1.Text = "Status: Valheim not found!";
+                button1.Enabled = false;
+                button1.Text = "PLAY";
+            }
+            else
+            {
+                if(Properties.Settings.Default.Installed == true)
+                {
+                    button1.Enabled = true;
+                    button1.Text = "PLAY";
+                    label1.Text = "Status: Ready to play!";
+                    textBox1.AppendText("-> Ready to play!" + Environment.NewLine);
+                    _timer.Dispose();
+                    return;
+                }
+                else
+                {
+                    textBox1.AppendText("-> Valheim found! " + Properties.Settings.Default.Path + Environment.NewLine);
+                    textBox1.AppendText("-> Ready for install!" + Environment.NewLine);
+                    label1.Text = "Status: Install pending..";
+                    button1.Enabled = true;
+                    button1.Text = "INSTALL";
+                    _timer.Dispose();
+                }
+            }
+        }
+
+        private void Download()
+        {
+            button1.Text = "DOWNLOADING";
+            button1.Enabled = false;
+
+            if(File.Exists(Properties.Settings.Default.Path + @"\\Mods.zip") == true)
+            {
+                textBox1.AppendText("-> Deleted old zip" + Environment.NewLine);
+                label1.Text = "Status: Cleaning up..";
+                try
+                {
+                    File.Delete(Properties.Settings.Default.Path + @"\\Mods.zip");
+                }
+                catch(Exception ex)
+                {
+                    
+                }              
+            }
+           var download_task = DownloadArchiveAsync(url);         
+        }
+
+        private void Unzip()
+        {
+            textBox1.AppendText("-> Unzipping.. " + Environment.NewLine);
+            using (Ionic.Zip.ZipFile zip = Ionic.Zip.ZipFile.Read(Properties.Settings.Default.Path + @"\\Mods.zip"))
+            {
+                zip.ExtractProgress +=
+                   new EventHandler<ExtractProgressEventArgs>(zip_ExtractProgress);
+                zip.ExtractAll(Properties.Settings.Default.Path, ExtractExistingFileAction.OverwriteSilently);
+                System.IO.DirectoryInfo di = new DirectoryInfo(Properties.Settings.Default.Path + "\\BepInEx\\plugins");
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    textBox1.AppendText("-> Installed Mod: " + file.Name + Environment.NewLine);
+                }
+
+            }
+            textBox1.AppendText("-> Unzipping completed " + Environment.NewLine);
+            label1.Text = "Status: Done..";
+            Properties.Settings.Default.Installed = true;
+            Properties.Settings.Default.Save();
+            button1.Enabled = true;
+            button1.Text = "PLAY";
+            label1.Text = "Status: Ready to play!";
+            textBox1.AppendText("-> Ready to play!" + Environment.NewLine);
+
+        }
+
+        void zip_ExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            if (e.TotalBytesToTransfer > 0)
+            {
+                progressBar1.Value = Convert.ToInt32(100 * e.BytesTransferred / e.TotalBytesToTransfer);
+                label1.Text = "Status: Files extracted " + e.CurrentEntry.ToString();
+            }
+        }
+
+        async Task<bool> DownloadArchiveAsync(string fileUrl)
+        {
+            var downloadLink = new Uri(fileUrl);
+
+            DownloadProgressChangedEventHandler DownloadProgressChangedEvent = (s, e) =>
+            {
+                progressBar1.BeginInvoke((Action)(() =>
+                {
+                    progressBar1.Value = e.ProgressPercentage;
+                }));
+
+                var downloadProgress = string.Format("{0} MB / {1} MB",
+                        (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                        (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+
+                label1.BeginInvoke((Action)(() =>
+                {
+                    label1.Text = ("Status: Downloading " + downloadProgress + " ..." + Environment.NewLine);
+                }));
+
+            };
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadProgressChanged += DownloadProgressChangedEvent;
+                await webClient.DownloadFileTaskAsync(downloadLink, Properties.Settings.Default.Path.ToString() + @"\\Mods.zip");
+                
+            }
+            label1.Text = "Status: Download Complete";
+            textBox1.AppendText("-> Downloaded" + Environment.NewLine);
+            Unzip();
+            return true;         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(Properties.Settings.Default.update == true)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Do you wish to update your mods?", "BananaCraft Mods updater", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        System.IO.DirectoryInfo di = new DirectoryInfo(Properties.Settings.Default.Path + "\\BepInEx\\plugins");
+
+                        foreach (FileInfo file in di.GetFiles())
+                        {
+                            label1.Text = file.FullName;
+                            file.Delete();
+                        }
+                        Properties.Settings.Default.update = false;
+                        Properties.Settings.Default.Save();
+                        Download();
+                        return;
+                    }
+                }
+                bool installed = Properties.Settings.Default.Installed;
+                if(installed == true)
+                {
+                    button1.Enabled = true;
+                    button1.Text = "PLAY";
+                    label1.Text = "Status: Ready to play";
+                    StartGame();
+                    Properties.Settings.Default.Server = textBox2.Text;
+                    Properties.Settings.Default.Arguments = toolStripTextBox1.Text;
+                    Properties.Settings.Default.Save();
+                    return;
+                }
+                else
+                {
+                    Download();
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.AppendText(ex.ToString());
+            }
+        }
+        private void uninstallToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string path = Properties.Settings.Default.Path;
+            try
+            {
+                label1.Text = "Status: Uninstalling";
+                textBox1.AppendText("-> Uninstalling" + Environment.NewLine);
+                CheckProcess(false);
+                Directory.Delete(path + "\\BepInEx", true);
+                Directory.Delete(path + "\\doorstop_libs", true);
+                Directory.Delete(path + "\\unstripped_corlib", true);
+                File.Delete(path + "\\winhttp.dll");
+                File.Delete(path + "\\doorstop_config.ini");
+                label1.Text = "Status: Uninstalled!";
+                textBox1.AppendText("-> Ready for installation!" + Environment.NewLine);
+                button1.Text = "INSTALL";
+                Properties.Settings.Default.Installed = false;
+                Properties.Settings.Default.Path = "";
+                Properties.Settings.Default.Save();
+                not_installed();
+            }
+            catch (Exception ex)
+            {
+                textBox1.AppendText("-> Mods are not installed!" + Environment.NewLine);
+                Properties.Settings.Default.Installed = false;
+                Properties.Settings.Default.Path = "";
+                Properties.Settings.Default.Save();
+            }
+        }
+        private void supportToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/FdU4CKM");
+        }
+    }
+}
